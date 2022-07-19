930631f07eee93a841745dfb937864aedfd3b3a7
lunaisnotaboy <her@mint.lgbt>
2022-07-18 21:05:46 -0400

    Add a helper for adding classes to the body

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # A list of all of the classes that should be
+  # included in the body.
+  #
+  # Returns a String of body classes.
+  def body_classes(classes = nil)
+    [
+      "env-#{Rails.env.downcase}",
+      (user_signed_in? ? 'logged-in' : 'logged-out'),
+      classes
+    ].join(' ').strip
+  end
 end
